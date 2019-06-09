8fbba5e58cf6a0a310567ac31c8e4665fd72f675
hrutkows <hubert.rutkowski@intel.com>
2019-06-10 00:17:25 +0200

    Some initial thoughts about parsing free-form gym.txt

---
+++ freeform.py
@@ -0,0 +1,174 @@
+
+""" Functions that help to parse quite free-form text file, and save it as yaml,
+ that will be interpreted by load_trainings_file()
+
+# todo: try out https://textx.github.io/Arpeggio/stable/
+
+
+Syntax cases mixed inside, that have to be taken into account by one grammar:
+
+2007-10-10
+    Przysiad: 10 (35)
+    Podciąganie na drążku: ~5
+
+2007-10-8    Klata
+    wyciskanie sztangi: 50/12 , 60/7 , 63/6
+    wyciskanie sztangielek: 20/10
+
+
+*2010-08-07 Klata*
+  wyciskania płaska 50/12  60/8  65/6  70/2
+  sztangielki skosna  12x15  10x20  6x22.5
+  rozpietki skosne 5/15  7.5/12  10/10
+  przenoszenie sztangielki 20 22.5 25
+
+2010-08-25
+nogi
+ przysiady 12x50 10x70 8x80 7x90 2x95
+ wyprosty siedzac 12x20 12x30 8x40 6x50
+ zginanie lezac 12x20 10x30 7x40 7x40
+ wyprosty na maszynie 5x90 5x115 5x135 5x155
+ łydki
+
+
+2012-07-19
+klata
+    płaska 60/12, 65/7
+    sztangielki skośna, rozpiętki
+    podciąganie podchwytem 10x +10kg, 6x +15kg
+
+
+2012-08-23
+plecy
+    MC sumo 60 rozgrz, 80x5, 100x5, 110x1, 120x1, 130x1, 135x1, 120x1, 110x1, 100x2 = 17 powtorzen, 1825kg
+    lawka rzymska 8-10 obciazenie +5, +10, +15
+    podciaganie podchwytem 5x +14, 4x +21, 3x +28, 3x +32, 1x +35
+    szrugsy kettleballs +17, +24, +32
+
+* ćwiczyłem prawie 2h, nie brakowało mi energii: owsianka, spaghetti, kreatyna
+* przy ciężkich przysiadach miałem asystę z tyłu, przydała się
+* widzę że muszę potrenować technikę, co robić po pode
+
+
+2012-12-11 02:48
+klata
+    wyciskanie na plaskiej 30/15, 40/10, 50/10, 60/10, 65/10+1
+    przytrzymanie na prostych 90, 100
+    rozpietki 8/11, 10/11, 11/10
+    przenoszenie sztangielki 21/10, 23/11
+
+
+*2013-08-09 powrot do formy  triceps*
+    martwy ciag 40/10, 60/10, 70/9, 80/8
+    uginanie francuskie sztangi 10/15, 15/12, 20/10
+    prostowanie nog 20, 30, 37.5, 45 / 10
+    szrugsy 15/15, 20/15, 25/10
+    kettle wymachy 8/12, 12/9, 16/8 1reka
+
+
+*2013-09-1  nogi triceps   58.3*
+    kettle wymachy   8/12, 12/12, 16/9 (obie!)
+    przysiady    20/16, 40/10, 50/10, 60/10, 70/7
+    wyprosty   37.5/10, 45/10, 52.5/10, 60/8
+    uginanie   30/12, 37.5/12, 45/11
+    wspiecia  0/20, 10/30, 20/30
+    ginekolog  45/10 x3
+
+    prostowanie sztangielki nad glowa   5/12, 7/11
+    uginanie francuskie sztangi     10/15, 15/12, 20/10
+    pompki szwedzkie 10, 12
+    pompki na piesciach waskie 10
+    pistolety 5, 5
+    zwijanie linki 10, 15, 20, 25
+
+
+*2014-02-21  nogi przod i tyl,  *
+    przysiady 20/15, 45/15, 55/15, 65/15
+    krecenie kolek nogami lezacymi na pilce   22, 24, 20, pojedyncze 12
+    cofanie sie z raczka wyciagu  42, 50, 57, 65
+    przysiady jednonozne, na kazda noge:  7-8/0,  6/10, 6/15
+    ginekolog 37/14, 45/12, 52/11   i drugi rodzaj  40/20
+    lydki
+        stojac 35, 40
+        na siedzaco 15/25, 20/24, 22.5/18
+
+*2014-06-17 FBW na przeprowadzke ;) waga 61.3*
+    MC 20/15, 50/15, 65/12, 75/10, 90/7
+    podciaganie 11 nsz, 10.5 nsz, 10 pw
+    uginanie ramion ze sztanga lamana 18/13, 23/11, 25.5/5+3.5
+    21 sztangielkami mlotkowo 5, 6, 7
+    przedramiona
+        uginanie sztangi z tylu 25/15, 35/9
+        zwijanie linki 2.5, 3.75
+    bieznia
+
+*2014-07-09 waga 60*
+
+*2015-05-17 klata bic*
+wycisk Sz plaska wezej 20/20, 40/14, 50/10+3, 60/5+3
+wycisk H skosna 12.5/15, 15/13, 17.5/13, 20/12
+przenoszenie H 17.5/10, 20/10, 22.5/10
+butterfly 20/12, 27/12, 35/12
+uginanie Sz na siedząco 18/15, 25.5/12, 28/9
+modlitewnik 34/10, 34/6, 41, 34,27/max
+uginanie o kolano koncentracja 10/8, 12.5/7, 15/5
+pompki szwedzkie 8, 7, 7
+
+
+*2016-01-03*  Heros gym  Waga 62.2
+płaska 20/20, 40/15, 50/13, 60/8, 30/20
+Skosna h 12.5/15, 15/13, 17.5 /10,  20/9
+Arnoldki 10 /13, 12.5 /10, 15/6+3
+Przysiady 20 /20, 40 /14, 60/12, 70/9
+Uginanie sztangi bic 20/15, 22.5/12, 25/10
+Podciaganie n 7+3, 5+3+2, p 9
+
+
+*5.2.2016* nogi
+Przysiady 10, 20/15, 40 /15, 60/12, 70/8, 80/6, 90/3
+Super seria
+Wyprosty
+Uginanie
+Ginekolog
+1 nozne 7, 7.5 /7, 10/
+Jefferson 40/10, 65/
+
+
+*2017-05-30 nogi*
+Przysiady 30/10, 50/12, 70/11, 80/8, 90/6
+Wykroki 12.5,  15, 17.5,  20
+
+26.11
+Przysiady 20/12 40/12 60/10 70/8
+Wyciskanie skosna 20/10 40/12 60/7 50/8
+Podciąganie podchwyt 10, 9, 7
+Wyciskanie zolnierskie 20/12, 30/10, 30/
+Zurawie
+
+---------- Syntax that has to be manually fixed in file:
+
+1 nozne 7, 7.5 /7, 10/
+Jefferson 40/10, 65/
+
+
+"""
+
+# grammar in BNF kinda:
+
+training_header="<*>DATE<*>  COMMENT <*>"
+after_training_header="COMMENT"
+
+exercise="<NAME>\s(<REPETITIONS> <SEPARATOR> )* "   # todo: for last repetition, separator is not needed
+name="<string>"
+
+repetitions="(WEIGHT/TIMES | TIMES | +WEIGHT) <MULTIPLIER>"
+
+weight="FLOAT"
+times="INT|INT\s+\s<after_short_break>"
+after_short_break="INT"
+
+separator = (",|space")
+
+multiplier = "x\s<NUMBER>"
+
+additional_weight = "+<NUMBER>(kg)"
